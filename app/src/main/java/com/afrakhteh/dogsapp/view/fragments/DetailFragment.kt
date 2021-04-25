3ef1548c813adf66e7714322abad8d7418b82096
melikaafrakhteh <45101381+melikaafrakhteh@users.noreply.github.com>
2021-04-25 16:00:07 +0430

    navigation works testing

--- app/src/main/java/com/afrakhteh/dogsapp/view/fragments/DetailFragment.kt
+++ app/src/main/java/com/afrakhteh/dogsapp/view/fragments/DetailFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.afrakhteh.dogsapp.R
+import kotlinx.android.synthetic.main.fragment_detail.*
 
 class DetailFragment : Fragment() {
 
@@ -13,6 +15,14 @@ class DetailFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        back_btn.setOnClickListener {
+            val action = DetailFragmentDirections.actionDetailFragmentToHomeFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 }
--- app/src/main/java/com/afrakhteh/dogsapp/view/fragments/HomeFragment.kt
+++ app/src/main/java/com/afrakhteh/dogsapp/view/fragments/HomeFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.afrakhteh.dogsapp.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -15,4 +17,11 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        next_btn.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToDetailFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
 }

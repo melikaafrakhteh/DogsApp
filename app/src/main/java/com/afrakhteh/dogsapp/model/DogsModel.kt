c1c2704e14f42b90cc8f95baa6dd250bbdde65d1
melikaafrakhteh <45101381+melikaafrakhteh@users.noreply.github.com>
2021-04-26 23:08:08 +0430

    some changing in detail ui and building the model

---
+++ app/src/main/java/com/afrakhteh/dogsapp/model/DogsModel.kt
@@ -0,0 +1,11 @@
+package com.afrakhteh.dogsapp.model
+
+data class DogsModel(
+        val dogId: String?,
+        val dogBread: String?,
+        val dogLifeSpan: String?,
+        val dogBreadGroup: String?,
+        val dogBreadFor: String?,
+        val temperament: String?,
+        val imageUrl: String?
+)

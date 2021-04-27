b68ec0b066ebf9d23a0b1ba744754af2372d9c3f
melikaafrakhteh <45101381+melikaafrakhteh@users.noreply.github.com>
2021-04-27 23:14:09 +0430

    setup service class for retrofit

--- app/src/main/java/com/afrakhteh/dogsapp/model/api/DogsApiService.kt
+++ app/src/main/java/com/afrakhteh/dogsapp/model/api/DogsApiService.kt
@@ -1,4 +1,23 @@
 package com.afrakhteh.dogsapp.model.api
 
+import com.afrakhteh.dogsapp.model.DogsModel
+import com.afrakhteh.dogsapp.utils.Constant
+import io.reactivex.Single
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
 class DogsApiService {
+
+    private val api = Retrofit.Builder()
+            .baseUrl(Constant.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+            .create(DogsApi::class.java)
+
+
+    fun getDogs(): Single<List<DogsModel>> {
+        return api.getDogs()
+    }
 }

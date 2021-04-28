af6de6e57077654c7c645b179f1924500aff6ed7
melikaafrakhteh <45101381+melikaafrakhteh@users.noreply.github.com>
2021-04-28 11:41:09 +0430

    set a file to get images

---
+++ app/src/main/java/com/afrakhteh/dogsapp/utils/ImageUtils.kt
@@ -0,0 +1,24 @@
+package com.afrakhteh.dogsapp.utils
+
+import android.content.Context
+import android.widget.ImageView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.afrakhteh.dogsapp.R
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+
+fun getProgressDrawable(context: Context): CircularProgressDrawable {
+    return CircularProgressDrawable(context).apply {
+        strokeWidth = 10f
+        centerRadius = 50f
+        start()
+    }
+}
+
+fun ImageView.load(uri: String?, progressDrawable: CircularProgressDrawable) {
+    val option = RequestOptions()
+            .placeholder(progressDrawable)
+            .error(R.drawable.error)
+
+    Glide.with(context).setDefaultRequestOptions(option).load(uri).into(this)
+}

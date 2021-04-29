e705a89b96a4e5d9640572b34d7193f3ac2cf0be
melikaafrakhteh <45101381+melikaafrakhteh@users.noreply.github.com>
2021-04-29 18:18:49 +0430

    set room database using singleton design pattern

---
+++ app/src/main/java/com/afrakhteh/dogsapp/model/database/DogsDatabasa.kt
@@ -0,0 +1,32 @@
+package com.afrakhteh.dogsapp.model.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.afrakhteh.dogsapp.model.datamodel.DogsModel
+import com.afrakhteh.dogsapp.utils.Constant
+
+
+@Database(entities = [DogsModel::class], version = Constant.DATABASE_VERSION)
+abstract class DogsDatabase : RoomDatabase() {
+    abstract fun getDao(): DogDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: DogsDatabase? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = INSTANCE ?: synchronized(LOCK) {
+            INSTANCE ?: buildDb(context).also {
+                INSTANCE = it
+            }
+        }
+
+        private fun buildDb(context: Context) = Room.databaseBuilder(
+                context.applicationContext,
+                DogsDatabase::class.java,
+                Constant.DATABASE_NAME)
+                .build()
+    }
+}
--- app/src/main/java/com/afrakhteh/dogsapp/utils/Constant.kt
+++ app/src/main/java/com/afrakhteh/dogsapp/utils/Constant.kt
@@ -6,5 +6,9 @@ class Constant {
 
         const val END_POINT_URL: String = "/DevTides/DogsApi/master/dogs.json"
         const val BASE_URL: String = "https://raw.githubusercontent.com"
+
+        const val DATABASE_NAME = "database_dog.db"
+        const val DATABASE_VERSION = 1
+
     }
 }
